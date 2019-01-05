16afb8d63f6b8e83a28cf902044c375b14454497
Miguel Mota <miguelmota2@gmail.com>
2019-01-04 18:22:25 -0800

    public key to address util

--- example/example.go
+++ example/example.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/miguelmota/go-ethutil"
+	ethutil "github.com/miguelmota/go-ethutil"
 )
 
 func main() {
+	publicKeyBytes, _ := hex.DecodeString("049a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05")
+	address := ethutil.PublicKeyBytesToAddress(publicKeyBytes)
+	fmt.Println(address.Hex()) // 0x96216849c49358B10257cb55b28eA603c874b05E
+
 	valid := ethutil.IsValidAddress("0x323b5d4c32345ced77393b3530b1eed0f346429d")
 	fmt.Println(valid) // true
 
